2a5c85c6ee5d19e3c21450bcd6791c2817521b52
ms0223900 <ms0223900@gmail.com>
2020-01-15 13:43:37 +0800

    fix: update export ColorPickerTool from index

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export { default as combineReducers } from './functions/combineReducers';
 export { default as connectCtx } from './functions/connectCtx';
 export { default as ContextWrapper } from './functions/ContextWrapper';
 export { default as contextHelpers } from './functions/contextHelpers';
+export { default as MaterialUIColorPickerTool } from './components/common-components/colorPickerTool/ColorPickerToolListContainer';
